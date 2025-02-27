22664bbc518c5c913b8813c9062c3a8dbc862337
nxhuel <nahueltisera03@gmail.com>
2025-02-27 18:24:33 -0300

    client-manager: Setting up project

---
+++ src/main/java/com/app/web/GestorClientesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.app.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorClientesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorClientesBackendApplication.class, args);
+	}
+
+}
